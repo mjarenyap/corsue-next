54b65399c94a4f2b45d60e178849747a8b43f8a6
Mike Jaren Yap <mjaren.yap@gmail.com>
2023-11-23 23:41:14 +0800

    added button and plasmic host

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,11 @@
 import { PlasmicComponent } from "@plasmicapp/loader-nextjs";
+import { Button } from "@/components/ui/button";
 
 export default function LoginPage() {
-  return <PlasmicComponent component="List" />;
+  return (
+    <>
+      <PlasmicComponent component="List" />
+      <Button>Login</Button>
+    </>
+  );
 }
